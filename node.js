4fa9119d83962d78806b4057411f99c3a1826871
JabSysEmb <abduljabbar.sha97@gmail.com>
2022-11-03 11:20:28 +0300

    get rid of axios, implemented with fetch

--- node.js
+++ node.js
@@ -1,16 +1,5 @@
 #!/usr/bin/env node
 
- const axios = require("axios");
-
- axios
- 	.get("http://api.aladhan.com/v1/timingsByCity?city=Istanbul&country=Turkey&method=13")
- 	.then(res => res.data)
- 	.then(data => data['data']['timings'])
- 	.then(timings => {
-		console.log(timings)
-		return timings;
-	})
-	.catch(
-		err => console.error(err)
-	);
-
+fetch("http://api.aladhan.com/v1/timingsByCity?city=Istanbul&country=Turkey&method=13")
+	.then(response => response.json())
+	.then(data => console.log(data['data']['timings']));
